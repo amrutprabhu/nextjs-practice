dc71b5fe9dcafc2864b28f0c1357b499295611dd
amrutprabhu <amrutprabhu42@gmail.com>
2022-08-06 16:24:54 +0200

    Added oauth integration

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
-import Users from '../components/users'
+import Nav from "../components/Nav";
+
 export default function Home() {
-   
   return (
     <div className="px-4 py-0">
-      <Users />
+      <Nav />
     </div>
-  )
+  );
 }
+
+// export async function getServerSideProps(context) {
+//   const session = await getSession(context);
+//   console.log("server ->");
+//   console.log(session);
+//   return {
+//     props: { session },
+//   };
+// }
